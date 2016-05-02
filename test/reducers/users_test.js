00c4f115954924dc2dc133fbef28c1a6f44c6ad8
Pei Xiong Liu <pliu@fiksu.com>
2016-05-01 21:15:38 -0400

    Add test for FETCH_USERS action

--- test/reducers/users_test.js
+++ test/reducers/users_test.js
@@ -6,4 +6,12 @@ describe('Users Reducer', () => {
   it ('handles action with unknown type', () => {
     expect( usersReducer(undefined, {}) ).to.eql([]);
   });
+
+  it ('handles action of type FETCH_USERS', () => {
+    const action = {
+      type: FETCH_USERS,
+      payload: [{ name: 'Sara' }]
+    };
+    expect( usersReducer([], action)).to.eql([{name: 'Sara'}]);
+  });
 });
