e16b0957d85fa331b356d49caef6a6f1de61662f
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-05-02 23:45:03 -0400

    Add actions index_test

---
+++ test/actions/index_test.js
@@ -0,0 +1,11 @@
+import { expect } from '../test_helper';
+import { FETCH_USERS } from '../../src/actions/types';
+import { fetchUsers } from '../../src/actions';
+
+describe('actions', () => {
+  describe('fetchUsers', () => {
+    it('has the correct type', () => {
+      expect(fetchUsers().type).to.eql('FETCH_USERS');
+    });
+  });
+});
